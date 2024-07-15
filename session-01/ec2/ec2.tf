fd265e05157e5a502c07dda2f254a19007a87936
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-15 17:05:39 +0530

    added SG related code

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,8 +1,36 @@
 resource "aws_instance" "name" {
-  ami           = "ami-0b4f379183e5706b9" #devops-practice
-  instance_type = "t3.micro"
+  ami                    = "ami-0b4f379183e5706b9" #devops-practice
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
+  name        = "roboshop-all-aws"             # this is for AWS
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow All ports"
+    from_port   = 0 # 0 means all ports
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-all-aws"
   }
 }
