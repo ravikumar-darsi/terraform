f3217cc1a60c122bf7f7aae7434de7e4f1735b76
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-25 20:58:29 +0530

    some info abt locals

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name = "Ravikumar"
+  name = "Ravikumar "
   training = "Learning Terraform"
   instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
   ip = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
