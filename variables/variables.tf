79fdab143aa5ed5c7e9be37b9ac82ec07f23c488
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-25 20:55:28 +0530

    files related to variables

---
+++ variables/variables.tf
@@ -0,0 +1,45 @@
+# 1. Command line
+# 2. -var-file=""
+# 3. terraform.tfvars
+# 4. Env variables
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    Name        = "Hello Terraform"
+    Project     = "roboshop"
+    Environment = "DEV"
+    Component   = "web"
+    Terraform   = "true"
+  }
+}
+
+variable "sg-name" {
+  type = string
+  default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "Allow TLS inbound traffic"
+}
+
+variable "inbound-from-port" {
+  #type = string
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type    = list(any)
+  default = ["0.0.0.0/0"]
+}
