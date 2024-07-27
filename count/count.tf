dd5f67a4bc00d9db7075340b52110d9bd17a6954
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-27 13:16:20 +0530

    small modificaton

--- count/count.tf
+++ count/count.tf
@@ -7,12 +7,12 @@ resource "aws_instance" "web" {
     Name = var.instance_names[count.index]
   }
 }
-
-resource "aws_route53_record" "www" {
+ 
+ resource "aws_route53_record" "www" {
   #count   = 11
   count = length(var.instance_names)
   zone_id = var.zone_id
-  name    = "${var.instance_names[count.index]}.${var.domain_name}" # interploation mixing variable and static value 
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" # interploation mixing two variable and static value 
   type    = "A"
   ttl     = 1
   records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
